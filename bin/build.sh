cac67d4a97f3bb09d20fcd89bdc261604bb2f333
John Webb <John.Webb94@gmail.com>
2025-09-14 12:35:11 -0700

    Move XCode build instructions to own script

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 
-pushd build
-cmake .. --preset conan-default
-popd
-
-# cmake --build build --preset conan-default
+cmake --preset conan-debug
+cmake --build build --preset conan-debug
--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 python3 -m .venv
 source ./.venv/bin/activate
 
-mkdir build
-pushd build
-
-conan install .. -s build_type=Debug -c tools.cmake.cmaketoolchain:generator=Xcode -of build
-conan install .. -s build_type=Release -c tools.cmake.cmaketoolchain:generator=Xcode -of build
-
-popd
-
-# conan install . --build=missing -s build_type=Debug
+conan install . --build=missing -s build_type=Debug
