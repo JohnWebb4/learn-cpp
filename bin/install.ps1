a1d1cd8ecc85d4af22483aab3857afc50542a9bc
John Webb <john.webb94@gmail.com>
2025-09-14 19:50:08 -0700

    Update powershell install script

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,3 +1,6 @@
+#!/usr/bin/env sh
+
 python3 -m .venv
 ./.venv/Scripts/Activate.ps1
-conan install . --build=missing -s build_type=Debug
+
+conan install . --build=missing -s build_type=Debug -of build
